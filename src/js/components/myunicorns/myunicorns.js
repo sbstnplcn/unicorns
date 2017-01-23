ef33d9a8632815cadf17810315b1b2ceeb80c27f
sbstnplcn <sebastien.palacin@gmail.com>
2017-01-23 17:59:04 +0100

    page my unicorns

--- src/js/components/myunicorns/myunicorns.js
+++ src/js/components/myunicorns/myunicorns.js
@@ -9,8 +9,13 @@
                         return unicornService.get()
                     }).then((res) => {
                         let allUnicorns = res.data
+                        allUnicorns.forEach((el) => {
+                            let a = new Date
+                            let b = new Date(el.birth)
+                            return el.age = (a.getFullYear() - b.getFullYear())
+                        })
                         this.unicorns = allUnicorns.filter((el, idx) => {
-                            return el.owner == this.user._id
+                            return el.owner._id == this.user._id
                         })
                     })
 
