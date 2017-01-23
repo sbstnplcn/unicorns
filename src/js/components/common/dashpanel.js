840a61139fa868ec7cee3681e9f82b1727602650
sbstnplcn <sebastien.palacin@gmail.com>
2017-01-23 17:30:17 +0100

    add radio button to crud unicorn

--- src/js/components/common/dashpanel.js
+++ src/js/components/common/dashpanel.js
@@ -1,12 +1,44 @@
 ((app) => {
     app.component('dashpanel', {
         templateUrl: "js/components/common/dashpanel.html",
-        controller: ['usersService', function(usersService) {
+        controller: ['usersService', 'authService', '$state', function(usersService, authService, $state) {
             angular.extend(this, {
                 $onInit() {
                     usersService.getCurrent().then((res) => {
                         this.user = res.data
                     })
+
+                    //Edit Mode
+                    this.editMode = false
+
+                    let previous = {}
+
+                    this.edit = (user) => {
+                        if (this.editMode) {
+                            usersService.edit(user).then((res) => {
+                                this.editMode = false
+                            })
+                        } else {
+                            previous[user] = angular.copy(user)
+                            this.editMode = true
+                        }
+                    }
+
+                    // cancel
+                    this.cancel = (user) => {
+                        user = previous[user]
+                        this.user = user
+                        this.editMode = false
+                    }
+
+                    // delete
+                    this.delete = (user) => {
+                        usersService.delete(user).then((res) => {
+                            $state.go('app.dashboard', {
+                                reload: true
+                            })
+                        })
+                    }
                 },
                 disconnect() {
                     authService.disconnect().then(() => {
